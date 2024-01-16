fb99df9e027033f39365140ce190457348d1a0cc
Mikerosoft99 <kerosmay8000@gmail.com>
2024-01-16 19:25:35 +0200

    Table Select updated

--- select_table.sh
+++ select_table.sh
@@ -11,13 +11,24 @@ if [ -e "$tablename" ]; then
     select option in "Select All" "Select by ID" "Select row" "Back to previous menu"; do
         case $REPLY in
         1)
-            exit 1
+            clear
+            awk 'NR!=2' "$tablename"
+            cd ../../
+            source table_menu.sh
             ;;
         2)
-            exit 1
+            read -p "Enter the ID to search: " search_id
+            clear
+            grep "^ID" "$tablename"
+            grep "^$search_id" "$tablename"
+            cd ../../
+            source table_menu.sh
             ;;
         3)
-            exit 1
+            clear
+            read -p "Enter a column name: " search_column
+            field_number=$(awk -F: -v word="$search_column" '{ for(i=1; i<=NF; i++) if($i ~ word) print i; exit }' "$tablename")
+            awk -F: -v field="$field_number" '{ print $field }' "$tablename"
             ;;
         4)
             cd ../../
